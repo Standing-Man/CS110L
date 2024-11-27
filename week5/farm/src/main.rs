f2d556062e669a470d2b8e7b3c004cab7728a4fd
Alan Tang <jmtangcs@gmail.com>
2024-11-27 20:23:50 +0800

    Pass Exercises: Farm meets multithreading

--- week5/farm/src/main.rs
+++ week5/farm/src/main.rs
@@ -1,8 +1,6 @@
 use std::collections::VecDeque;
-#[allow(unused_imports)]
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
-#[allow(unused_imports)]
 use std::{env, process, thread};
 
 /// Determines whether a number is prime. This function is taken from CS 110 factor.py.
@@ -25,7 +23,6 @@ fn is_prime(num: u32) -> bool {
 /// from CS 110 factor.py.
 ///
 /// You don't need to read or understand this code.
-#[allow(dead_code)]
 fn factor_number(num: u32) {
     let start = Instant::now();
 
@@ -66,17 +63,43 @@ fn get_input_numbers() -> VecDeque<u32> {
     numbers
 }
 
+fn helper(queue: Arc<Mutex<VecDeque<u32>>>) -> Option<u32> {
+    let mut unlocked_queue = queue.lock().unwrap();
+    unlocked_queue.pop_back()
+}
+
 fn main() {
     let num_threads = num_cpus::get();
     println!("Farm starting on {} CPUs", num_threads);
     let start = Instant::now();
 
     // TODO: call get_input_numbers() and store a queue of numbers to factor
+    let queue = get_input_numbers();
+
+    let data  = Arc::new(Mutex::new(queue));
+
+    let mut handles = vec![];
+    
 
     // TODO: spawn `num_threads` threads, each of which pops numbers off the queue and calls
     // factor_number() until the queue is empty
 
+    for _ in 0..num_threads {
+        let data_clone = Arc::clone(&data);
+        let handle = thread::spawn(move || {
+            let num = helper(data_clone);
+            if num.is_some() {
+                factor_number(num.unwrap());   
+            }
+        });
+        handles.push(handle);
+    } 
+
     // TODO: join all the threads you created
+
+    for handle in handles {
+        handle.join().unwrap(); 
+    }
 
     println!("Total execution time: {:?}", start.elapsed());
 }
